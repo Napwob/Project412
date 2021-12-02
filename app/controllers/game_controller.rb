2cbcc47b7f680b69eef52785f1436f4b2e532556
Kadconner <47683913+Kadconner@users.noreply.github.com>
2021-12-02 16:23:21 +0700

    update game controller.rb

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -31,4 +31,34 @@ class GameController < ApplicationController
     flash.notice = current_user.go_job
     redirect_to '/game'
   end
+
+  def contemplate_nature
+    flash.notice = current_user.contemplate_nature
+    redirect_to '/game'
+  end
+
+  def drink_wine_and_watch_tv_series
+    flash.notice = current_user.drink_wine_and_watch_tv_series
+    redirect_to '/game'
+  end
+
+  def go_to_the_bar
+    flash.notice = current_user.go_to_the_bar
+    redirect_to '/game'
+  end
+
+  def drink_with_marginal_people
+    flash.notice = current_user.drink_with_marginal_people
+    redirect_to '/game'
+  end
+
+  def sing_in_the_subway
+    flash.notice = current_user.sing_in_the_subway
+    redirect_to '/game'
+  end
+
+  def sleep
+    flash.notice = current_user.sleep
+    redirect_to '/game'
+  end
 end
