520f2a9402b0a316a446015a182d08dd08affce4
Napwob <gorno-altaisk0@mail.ru>
2021-12-03 16:21:17 +0700

    Add help template

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,34 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_202_125_629) do
-  create_table 'slots', force: :cascade do |t|
-    t.integer 'user_id'
-    t.string 'name'
-    t.integer 'health'
-    t.integer 'mana'
-    t.integer 'happiness'
-    t.integer 'fatigue'
-    t.integer 'money'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 2021_12_02_125629) do
+
+  create_table "slots", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.integer "health"
+    t.integer "mana"
+    t.integer "happiness"
+    t.integer "fatigue"
+    t.integer "money"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'name'
-    t.integer 'health', default: 100
-    t.integer 'mana', default: 0
-    t.integer 'happiness', default: 0
-    t.integer 'fatigue', default: 0
-    t.integer 'money', default: 1000
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.integer "health", default: 100
+    t.integer "mana", default: 0
+    t.integer "happiness", default: 0
+    t.integer "fatigue", default: 0
+    t.integer "money", default: 1000
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
