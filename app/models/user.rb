de667b70661f01e5b60cef84ea302e9f1797eda9
Napwob <gorno-altaisk0@mail.ru>
2021-12-04 17:48:48 +0700

    Fix rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
   def load_from_slot(slot)
     self.health = slot.health
     self.mana = slot.mana
@@ -21,7 +19,6 @@ class User < ApplicationRecord
     self.happiness = 5
     self.fatigue = 0
     self.money = 5000
-
     save
   end
 
@@ -31,16 +28,11 @@ class User < ApplicationRecord
     self.happiness += happiness
     self.fatigue += fatigue
     self.money += money
-
     fix_stats
     save
   end
 
-  def fix_stats
-    self.health = 100 if self.health > 100
-
-    self.health = 0 if self.health.negative?
-
+  def fix_mana
     if self.mana.negative?
       self.mana = 0
       self.happiness -= 1
@@ -50,6 +42,13 @@ class User < ApplicationRecord
       self.health = self.health - ((self.mana - 100) / 4)
       self.mana = 100
     end
+  end
+
+  def fix_stats
+    fix_mana
+
+    self.health = 100 if self.health > 100
+    self.health = 0 if self.health.negative?
 
     self.fatigue = 100 if self.fatigue > 100
     self.fatigue = 0 if self.fatigue.negative?
@@ -61,11 +60,11 @@ class User < ApplicationRecord
     if self.money > 30_000
       save
 
-      'You win! Now Valera can buy notebook and work at home, drinking while working.'
+      'Valera say: Finally!'
     elsif (self.happiness < -9) || (self.health < 1)
       save
 
-      'You defeated! Valera was found dead.'
+      'Valera say: Farewell, cruel world...'
     else
       ''
     end
@@ -73,64 +72,64 @@ class User < ApplicationRecord
 
   def go_job
     if (mana > 50) || (fatigue > 60)
-      'Valera cannot go to work because of his condition'
+      'Valera say: Jjob?... W-what is it?'
     else
       apply_stats(0, -30, 0, 30, 1250)
-      'Valera goes to work'
+      'Valera say: Its been a hard day'
     end
   end
 
   def contemplate_nature
     apply_stats(0, -10, 1, -10, 0)
-    'Valera contemplates nature'
+    'Valera say: I wandered lonely as a cloud'
   end
 
   def drink_wine_and_watch_tv_series
     if money < 20
-      'Valera cannot drink wine and watch TV series, as he does not have enough money'
+      'Valera say: Sorry, out of money'
     else
       apply_stats(-5, 30, -1, 10, -20)
-      'Valera drinks wine and watches the TV series'
+      'Valera say: Ta-ta-tadada-ta...'
     end
   end
 
   def go_to_the_bar
     if money < 100
-      'Valera cannot go to the bar due to the fact that he does not have enough money'
+      'Valera say: Sorry, out of money'
     else
       apply_stats(-10, 60, 1, 40, -100)
-      'Valera goes to the bar'
+      'Valera say: Beer or not two beer?'
     end
   end
 
   def drink_with_marginal_people
     if money < 150
-      'Valera cannot drink with marginal people, due to the fact that he does not have enough money'
+      'Valera say: Sorry, out of money'
     else
       apply_stats(-80, 90, 5, 80, -150)
-      'Valera drinks with marginal people'
+      'Valera say: Oj, MOROZ MOROOOOOZ...'
     end
   end
 
   def sing_in_the_subway
     if fatigue > 80
-      'Valera cannot sing on the subway, as she is very tired'
-    elsif (mana > 40) && (mana < 70)
+      'Valera say: I need to sleep, not sing'
+    elsif mana.between?(60, 30)
       apply_stats(0, 0, 0, 0, +50)
-      'Valera sing in the subway really nice'
+      'Valera say: Near, far, wherever you are...'
     else
       apply_stats(0, -10, 1, 20, +10)
-      'Valera sing in the subway not bad'
+      'Valera say: IM GONNA ROCK!!!'
     end
   end
 
   def sleep
     if mana < 40
       apply_stats(90, -50, 0, -70, 0)
-      'Valera is sleeping good'
+      'Valera say: zZzZzZ...'
     else
       apply_stats(0, -50, -3, -70, 0)
-      'Valera is sleeping bad'
+      'Valera say: zzz...one more shot...zzz'
     end
   end
 end
