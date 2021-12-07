c3dacf976e0f3dd08579e540af9aa64928186ecf
Kadconner <47683913+Kadconner@users.noreply.github.com>
2021-12-07 16:44:01 +0700

    Mega Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
+require_relative 'user_actions'
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-
+  include UserActions
   def load_from_slot(slot)
     self.health = slot.health
     self.mana = slot.mana
@@ -67,68 +68,6 @@ class User < ApplicationRecord
       'Valera say: Farewell, cruel world...'
     else
       ''
-    end
-  end
-
-  def go_job
-    if (mana > 50) || (fatigue > 60)
-      'Valera say: Jjob?... W-what is it?'
-    else
-      apply_stats(0, -30, 0, 30, 1250)
-      'Valera say: Its been a hard day'
-    end
-  end
-
-  def contemplate_nature
-    apply_stats(0, -10, 1, -10, 0)
-    'Valera say: I wandered lonely as a cloud'
-  end
-
-  def drink_wine_and_watch_tv_series
-    if money < 200
-      'Valera say: Sorry, out of money'
-    else
-      apply_stats(-5, 30, 1, 10, -20)
-      'Valera say: Ta-ta-tadada-ta...'
-    end
-  end
-
-  def go_to_the_bar
-    if money < 250
-      'Valera say: Sorry, out of money'
-    else
-      apply_stats(-10, 60, 2, 40, -100)
-      'Valera say: Beer or not two beer?'
-    end
-  end
-
-  def drink_with_marginal_people
-    if money < 1500
-      'Valera say: Sorry, out of money'
-    else
-      apply_stats(-80, 90, 5, 80, -1500)
-      'Valera say: Oj, MOROZ MOROOOOOZ...'
-    end
-  end
-
-  def sing_in_the_subway
-    'Valera say: I need to sleep, not sing' if fatigue > 80
-    if (mana > 30) && (mana < 70)
-      apply_stats(0, 0, 0, 0, +50)
-      'Valera say: Near, far, wherever you are...'
-    else
-      apply_stats(0, -10, 1, 20, +10)
-      'Valera say: IM GONNA ROCK!!!'
-    end
-  end
-
-  def sleep
-    if mana < 40
-      apply_stats(90, -50, 0, -70, 0)
-      'Valera say: zZzZzZ...'
-    else
-      apply_stats(0, -50, -3, -70, 0)
-      'Valera say: zzz...one more shot...zzz'
     end
   end
 end
