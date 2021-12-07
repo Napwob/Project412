cfbc23553b2fcba74c3f23a7bb14a5418dc99f11
Napwob <gorno-altaisk0@mail.ru>
2021-12-07 21:18:50 +0700

    experiment

--- db/migrate/20211130124310_create_users.rb
+++ db/migrate/20211130124310_create_users.rb
@@ -3,5 +3,13 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users, &:timestamps
+    change_table :users, bulk: true do |t|
+      t.integer :health, default: 100
+      t.integer :mana, default: 30
+      t.integer :happiness, default: 5
+      t.integer :fatigue, default: 0
+      t.integer :money, default: 5000
+      t.string :name
+    end
   end
 end
