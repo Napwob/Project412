ac73adf8fef000ea5e6b5984e9000da3da586054
Napwob <gorno-altaisk0@mail.ru>
2021-12-08 22:37:09 +0700

    Finally some acceptance test

--- spec/features/user_actions_spec.rb
+++ spec/features/user_actions_spec.rb
@@ -1,8 +1,8 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.feature "User click on", :type => :feature do
-  scenario "Go Job" do
-    create:user, email:'person@example.com', password:'password'
+RSpec.describe 'User click on', type: :feature do
+  it 'Go Job' do
+    create :user, email: 'person@example.com', password: 'password'
 
     sign_in_with 'person@example.com', 'password'
 
@@ -11,12 +11,10 @@ RSpec.feature "User click on", :type => :feature do
     expect(page).to have_selector(:link_or_button, 'Game')
   end
 
-
-
   def sign_in_with(email, password)
     visit new_user_session_path
-    fill_in 'Email' ,with: email
-    fill_in 'Password' ,with: password
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
     click_button 'Log in'
   end
 end
