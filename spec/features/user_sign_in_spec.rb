f49c83c5c2d3e53bc37892b3be6b017f9a61464c
Napwob <gorno-altaisk0@mail.ru>
2021-12-08 23:55:56 +0700

    Sign in test rubocop

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Signing in', type: :feature do
   before do
-    create:user,email:'person@example.com',password:'password'
+    create :user, email: 'person@example.com', password: 'password'
   end
 
-  scenario 'notifies the user if his email or password is invalid' do
+  it 'notifies the user if his email or password is invalid' do
     sign_in_with 'person@example.com', 'wrong password'
     visit root_path
     expect(page).to have_selector(:link_or_button, 'Please, sign in to play')
   end
 
-
-  scenario 'signs the user in successfully with a valid email and password' do
+  it 'signs the user in successfully with a valid email and password' do
     sign_in_with 'person@example.com', 'password'
     visit root_path
     expect(page).to have_selector(:link_or_button, 'Game')
@@ -25,8 +24,7 @@ RSpec.describe 'Signing in', type: :feature do
     click_button 'Log in'
   end
 
-  #def user_sees_notice(text)
-    #expect(page).to have_css 'flash.alert',text: text
-  #end
-
+  # def user_sees_notice(text)
+  # expect(page).to have_css 'flash.alert',text: text
+  # end
 end
