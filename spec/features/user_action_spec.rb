b51ec3adcca7663ede6322cf5029f55472c478d6
Napwob <gorno-altaisk0@mail.ru>
2021-12-10 21:01:01 +0700

    Fixes

--- spec/features/user_action_spec.rb
+++ spec/features/user_action_spec.rb
@@ -12,75 +12,77 @@ describe 'User' do
     expect(page).to have_css('p', text: 'Money: 5000₽')
   end
 
-  it 'click Save Game' do
-    visit new_slot_path
-    fill_in 'Name save', with: 'Save 1'
-    click_button 'Save Game'
-    expect(page).to have_content('Valera Life Stats')
-  end
+  describe '.Click action' do
+    it 'Save Game' do
+      visit new_slot_path
+      fill_in 'Name save', with: 'Save 1'
+      click_button 'Save Game'
+      expect(page).to have_content('Valera Life Stats')
+    end
 
-  it 'click Load Game after save' do
-    visit new_slot_path
-    fill_in 'Name save', with: 'Save 1'
-    click_button 'Save Game'
-    visit slots_path
-    expect(page).to have_content('Save 1')
-  end
+    it 'Load Game after save' do
+      visit new_slot_path
+      fill_in 'Name save', with: 'Save 1'
+      click_button 'Save Game'
+      visit slots_path
+      expect(page).to have_content('Save 1')
+    end
 
-  it 'click Save Game and use empty Save Name' do
-    visit new_slot_path
-    fill_in 'Name save', with: ''
-    click_button 'Save Game'
-    expect(page).to have_content('Invalid name save!')
-    # expect(page).to have_css 'flash.alert',text:'Invalid name save!'
-  end
+    it 'Save Game and use empty Save Name' do
+      visit new_slot_path
+      fill_in 'Name save', with: ''
+      click_button 'Save Game'
+      expect(page).to have_content('Invalid name save!')
+      # expect(page).to have_css 'flash.alert',text:'Invalid name save!'
+    end
 
-  it 'click Go Job' do
-    make_default_stats
-    click_link('Go Job')
-    expect(page).to have_content('Valera say: Its been a hard day') and have_content('Money: 6250₽')
-    # expect(page).to have_content('Money: 6250₽')
-  end
+    it 'Go Job' do
+      make_default_stats
+      click_link('Go Job')
+      expect(page).to have_content('Valera say: Its been a hard day') and have_content('Money: 6250₽')
+      # expect(page).to have_content('Money: 6250₽')
+    end
 
-  it 'click Contemplate nature' do
-    make_default_stats
-    click_link('Contemplate nature')
-    expect(page).to have_content('Valera say: I wandered lonely as a cloud')
-  end
+    it 'Contemplate nature' do
+      make_default_stats
+      click_link('Contemplate nature')
+      expect(page).to have_content('Valera say: I wandered lonely as a cloud')
+    end
 
-  it 'click Drink wine and watch TV series' do
-    make_default_stats
-    click_link('Drink wine and watch TV series')
-    expect(page).to have_content('Valera say: Ta-ta-tadada-ta...') and have_content('Money: 4800₽')
-    # expect(page).to have_content('Money: 4800₽')
-  end
+    it 'Drink wine and watch TV series' do
+      make_default_stats
+      click_link('Drink wine and watch TV series')
+      expect(page).to have_content('Valera say: Ta-ta-tadada-ta...') and have_content('Money: 4800₽')
+      # expect(page).to have_content('Money: 4800₽')
+    end
 
-  it 'click Go to the bar' do
-    make_default_stats
-    click_link('Go to the bar')
-    expect(page).to have_content('Valera say: Beer or not two beer?') and have_content('Money: 4750₽')
-    # expect(page).to have_content('Money: 4750₽')
-  end
+    it 'Go to the bar' do
+      make_default_stats
+      click_link('Go to the bar')
+      expect(page).to have_content('Valera say: Beer or not two beer?') and have_content('Money: 4750₽')
+      # expect(page).to have_content('Money: 4750₽')
+    end
 
-  it 'click Drink with marginal people' do
-    make_default_stats
-    click_link('Drink with marginal people')
-    expect(page).to have_content('Valera say: Oj, MOROZ MOROOOOOZ...') and have_content('Money: 3500₽')
-    # expect(page).to have_content('Money: 3500₽')
-  end
+    it 'Drink with marginal people' do
+      make_default_stats
+      click_link('Drink with marginal people')
+      expect(page).to have_content('Valera say: Oj, MOROZ MOROOOOOZ...') and have_content('Money: 3500₽')
+      # expect(page).to have_content('Money: 3500₽')
+    end
 
-  it 'click Sing in the subway' do
-    make_default_stats
-    click_link('Sing in the subway')
-    expect(page).to have_content('Valera say: IM GONNA ROCK!!!') and have_content('Money: 5010₽')
-    # expect(page).to have_content('Money: 5010₽')
-  end
+    it 'Sing in the subway' do
+      make_default_stats
+      click_link('Sing in the subway')
+      expect(page).to have_content('Valera say: IM GONNA ROCK!!!') and have_content('Money: 5010₽')
+      # expect(page).to have_content('Money: 5010₽')
+    end
 
-  it 'click Sleep' do
-    make_default_stats
-    click_link('Sleep')
-    expect(page).to have_content('Valera say: zZzZzZ...') and have_content('Money: 5000₽')
-    # expect(page).to have_content('Money: 5000₽')
+    it 'Sleep' do
+      make_default_stats
+      click_link('Sleep')
+      expect(page).to have_content('Valera say: zZzZzZ...') and have_content('Money: 5000₽')
+      # expect(page).to have_content('Money: 5000₽')
+    end
   end
 
   def create_user(name, email, password)
