3ca2863c960c8dc287625791bcc97cb65e905955
Napwob <gorno-altaisk0@mail.ru>
2021-12-13 00:04:00 +0700

    Full cover

--- spec/features/user_action_spec.rb
+++ spec/features/user_action_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
-describe 'Actions' do
+describe 'Game' do
   before do
     create_user 'Username', 'person@example.com', 'password'
     visit game_path
+  end
+
+  it 'when unloged user get into' do
+    click_link('Sign Out')
+    visit game_path
+    user_sees_notice 'Error! You not authorized.'
   end
 
   it 'check correctance of stats initialisations' do
@@ -12,28 +18,59 @@ describe 'Actions' do
   end
 
   describe '#Save Game' do
-    it 'when user click Save Game and use non-empty savename' do
+    before do
       visit new_slot_path
+    end
+
+    it 'when user click Save Game and use non-empty savename' do
       fill_in 'Name save', with: 'Save 1'
       click_button 'Save Game'
       expect(page).to have_current_path(game_path, ignore_query: true)
     end
 
-    it 'when user click Save Game and use empty savename' do
+    it 'when user click Save Game and rewrite save' do
+      save_game 'Save_1'
       visit new_slot_path
+      save_game 'Save_1'
+      expect(page).to have_current_path(game_path, ignore_query: true)
+    end
+
+    it 'when user click Save Game and use empty savename' do
       fill_in 'Name save', with: ''
       click_button 'Save Game'
       user_sees_notice 'Invalid name save!'
     end
+
+    it 'when unloged user get into' do
+      click_link('Sign Out')
+      visit new_slot_path
+      user_sees_notice 'Error! You not authorized.'
+    end
   end
 
   describe '#Load Game' do
-    it 'when user click Load Game after save' do
+    before do
       visit new_slot_path
       fill_in 'Name save', with: 'Save 1'
       click_button 'Save Game'
+    end
+
+    it 'when user click Load Game after save' do
       visit slots_path
       expect(page).to have_content('Save 1')
+    end
+
+    it 'when user load old save' do
+      click_link 'Go Job'
+      visit slots_path
+      click_button 'Load Game'
+      expect(page).to have_content('Money: 5000₽')
+    end
+
+    it 'when unloged user get into' do
+      click_link('Sign Out')
+      visit slots_path
+      user_sees_notice 'Error! You not authorized.'
     end
   end
 
@@ -45,52 +82,54 @@ describe 'Actions' do
     end
   end
 
-  describe '#Go Job' do
-    it 'when user click Go Job' do
-      click_link 'Go Job'
-      user_sees_notice 'Valera say: Its been a hard day'
+  describe '.GameActions' do
+    describe '#Go Job' do
+      it 'when user click Go Job' do
+        click_link 'Go Job'
+        user_sees_notice 'Valera say: Its been a hard day'
+      end
     end
-  end
 
-  describe '#Contemplate nature' do
-    it 'when user click Contemplate nature' do
-      click_link 'Contemplate nature'
-      user_sees_notice 'Valera say: I wandered lonely as a cloud'
+    describe '#Contemplate nature' do
+      it 'when user click Contemplate nature' do
+        click_link 'Contemplate nature'
+        user_sees_notice 'Valera say: I wandered lonely as a cloud'
+      end
     end
-  end
 
-  describe '#Drink wine and watch TV series' do
-    it 'when user click Drink wine and watch TV series' do
-      click_link 'Drink wine and watch TV series'
-      user_sees_notice 'Valera say: Ta-ta-tadada-ta...'
+    describe '#Drink wine and watch TV series' do
+      it 'when user click Drink wine and watch TV series' do
+        click_link 'Drink wine and watch TV series'
+        user_sees_notice 'Valera say: Ta-ta-tadada-ta...'
+      end
     end
-  end
 
-  describe '#Go to the bar' do
-    it 'when user click Go to the bar' do
-      click_link 'Go to the bar'
-      user_sees_notice 'Valera say: Beer or not two beer?'
+    describe '#Go to the bar' do
+      it 'when user click Go to the bar' do
+        click_link 'Go to the bar'
+        user_sees_notice 'Valera say: Beer or not two beer?'
+      end
     end
-  end
 
-  describe '#Drink with marginal people' do
-    it 'when user click Drink with marginal people' do
-      click_link 'Drink with marginal people'
-      user_sees_notice 'Valera say: Oj, MOROZ MOROOOOOZ...'
+    describe '#Drink with marginal people' do
+      it 'when user click Drink with marginal people' do
+        click_link 'Drink with marginal people'
+        user_sees_notice 'Valera say: Oj, MOROZ MOROOOOOZ...'
+      end
     end
-  end
 
-  describe '#Sing in the subway' do
-    it 'when user click Sing in the subway' do
-      click_link 'Sing in the subway'
-      user_sees_notice 'Valera say: IM GONNA ROCK!!!'
+    describe '#Sing in the subway' do
+      it 'when user click Sing in the subway' do
+        click_link 'Sing in the subway'
+        user_sees_notice 'Valera say: IM GONNA ROCK!!!'
+      end
     end
-  end
 
-  describe '#Sleep' do
-    it 'when user click Sleep' do
-      click_link('Sleep')
-      user_sees_notice 'Valera say: zZzZzZ...'
+    describe '#Sleep' do
+      it 'when user click Sleep' do
+        click_link('Sleep')
+        user_sees_notice 'Valera say: zZzZzZ...'
+      end
     end
   end
 
@@ -112,6 +151,11 @@ describe 'Actions' do
   def click_game
     visit root_path
     click_button 'Game'
+  end
+
+  def save_game(text)
+    fill_in 'Name save', with: text
+    click_button 'Save Game'
   end
 
   def user_sees_notice(text)
--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -5,28 +5,29 @@ describe 'Path' do
 
   before do
     create :user, name: 'Username', email: 'person@example.com', password: 'password'
+    create :user, name: 'Usernamewin', email: 'personwin@example.com', password: 'passwordwin', money: '55000'
   end
 
   describe '#new_user_session_path' do
-    it 'user signing in using invalid password' do
+    it 'when user signing in using invalid password' do
       sign_in_with 'person@example.com', 'wrong password'
       expect(page).to have_current_path(new_user_session_path, ignore_query: true)
     end
 
-    it 'user signing in using valid email and password' do
+    it 'when user signing in using valid email and password' do
       sign_in_with 'person@example.com', 'password'
       expect(page).to have_current_path(root_path, ignore_query: true)
     end
   end
 
   describe '#destroy_user_session_path' do
-    it 'user signing out' do
+    it 'when user signing out' do
       sign_in_with 'person@example.com', 'password'
       click_link('Sign Out')
       expect(page).to have_current_path(root_path, ignore_query: true)
     end
 
-    it 'user singing out after getting into Game' do
+    it 'when user singing out after getting into Game' do
       sign_in_with 'person@example.com', 'password'
       click_link('Game')
       click_link('Sign Out')
@@ -35,15 +36,23 @@ describe 'Path' do
   end
 
   describe '#edit_user_registration_path' do
-    it 'user edit own profile' do
+    before do
       sign_in_with 'person@example.com', 'password'
+    end
+
+    it 'when user edit profile Name' do
       edit_data_with 'Username1', 'person@example.com', 'password'
+      expect(page).to have_current_path(root_path, ignore_query: true)
+    end
+
+    it 'when user edit profile Email' do
+      edit_data_with 'Username1', 'person1@example.com', 'password'
       expect(page).to have_current_path(root_path, ignore_query: true)
     end
   end
 
   describe '#help_path' do
-    it 'user click Help' do
+    it 'when user click Help' do
       sign_in_with 'person@example.com', 'password'
       click_link('Help')
       expect(page).to have_current_path(help_path, ignore_query: true)
@@ -51,18 +60,31 @@ describe 'Path' do
   end
 
   describe '#game_path' do
-    it 'user click Game' do
+    it 'when user click Valera button' do
       sign_in_with 'person@example.com', 'password'
-      click_link('Game')
+      visit game_path
+      click_link 'Valera'
+      expect(page).to have_current_path(root_path, ignore_query: true)
+    end
+
+    it 'when user win' do
+      sign_in_with 'personwin@example.com', 'passwordwin'
+      visit game_path
       expect(page).to have_current_path(game_path, ignore_query: true)
     end
   end
 
   describe '#root_path' do
-    it 'unloged user click Please, sign in to play' do
+    it 'when unlogged user click Please, sign in to play' do
       visit root_path
       click_link 'Please, sign in to play'
       expect(page).to have_current_path(new_user_session_path, ignore_query: true)
+    end
+
+    it 'when logged user click Game' do
+      sign_in_with 'person@example.com', 'password'
+      click_link 'Game'
+      expect(page).to have_current_path(game_path, ignore_query: true)
     end
   end
 
